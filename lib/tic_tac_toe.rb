fceac51f50a5821690b4674fb4813dea608a5919
Kenny Hollaway <hollawaykenny007@gmail.com>
2020-09-29 23:25:35 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -48,6 +48,13 @@ class TicTacToe
    return turn
   end
 
-    def current_player
-      
+  def current_player
+      num_turns = turn_count
+  if num_turns % 2 == 0
+    player = "X"
+  else
+    player = "O"
+  end
+  return player
+end 
 end
