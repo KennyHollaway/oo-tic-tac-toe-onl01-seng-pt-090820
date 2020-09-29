596ba6227215e50dbc7540f5b94a03df756273c4
Kenny Hollaway <hollawaykenny007@gmail.com>
2020-09-29 23:13:55 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -39,11 +39,9 @@ class TicTacToe
   end
   
   def turn_count
-    number_of_turns = 0
-    @board.each do |space|
-    if space == "X" || space == "O"
-       number_of_turns += 1
-    end
+    number_of_turns = 0 
+    
+    
   end
   
     
